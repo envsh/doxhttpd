f6aa9baa7b9a1a0be3a005160ed8402f58aa4bcb
egitop <egitop@uroad.xyz>
2026-09-14 10:11:11 +0800

    fix osx i18n

--- qltox/buildosx.sh
+++ qltox/buildosx.sh
@@ -2,14 +2,18 @@ set -e fail
 set -x
 
 export PKG_CONFIG_PATH=/opt/vcpkg/installed/x64-osx/lib/pkgconfig
+QMAKE=${QMAKE:-/opt/qt/5.15.2/clang_64/bin/qmake}
 QMAKE_EXTRA=""
 if [ x"$1" == x"asan" ]; then
     QMAKE_EXTRA="CONFIG+=asan"
 fi
-/opt/qt/5.15.2/clang_64/bin/qmake -r $QMAKE_EXTRA
+"$QMAKE" -r $QMAKE_EXTRA
 make
 
 # package
+mkdir -p qltox.app/Contents/Resources
+QT_TRANS=$("$QMAKE" -query QT_INSTALL_TRANSLATIONS)
+cp -f "$QT_TRANS/qt_zh_CN.qm" "$QT_TRANS/qt_zh_TW.qm" qltox.app/Contents/Resources/ 2>/dev/null || true
 tar zcf qltox-qt5-osx-x64.tar.gz qltox.app/
 ls -lh qltox-*.gz
 ls -lh qltox.app/Contents/MacOS/
